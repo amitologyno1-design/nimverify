7368cf7c8b067fde15d38fbe5e017cbf8257e57a
amitologyno1-design <YOUR_GITHUB_EMAIL>
2026-07-07 02:30:28 +0100

    Added login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from .forms import UserRegisterForm
 
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
+
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("dashboard")
+            form.save()
+            return redirect("login")
+
     else:
         form = UserRegisterForm()
 
     return render(request, "home/register.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
+
+    return render(request, "home/login.html")
