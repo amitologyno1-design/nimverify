860a5ed46a79861ec3a216a20b1558ccba09a1de
amitologyno1-design <YOUR_GITHUB_EMAIL>
2026-07-07 15:47:33 +0100

    Fixed home navigation

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
+    path("", views.home, name="home"),
+    path("register/", lambda request: redirect("register")),
+    path("login/", lambda request: redirect("login")),
 ]
